b4f4a66740d58921ce84b84e506bee1ddd7e2c5c
yanisa <75199035+bowyani@users.noreply.github.com>
2026-07-23 22:55:53 +0700

    Refactor App component with React Router setup

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
+
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import ProjectDetail from "./pages/ProjectDetail";
+
+
+function App() {
+
+  return (
+
+    <BrowserRouter>
+
+      <Routes>
+
+        <Route
+          path="/"
+          element={<Home />}
+        />
+
+
+        <Route
+          path="/projects"
+          element={<Projects />}
+        />
+
+
+        <Route
+          path="/projects/:id"
+          element={<ProjectDetail />}
+        />
+
+
+      </Routes>
+
+    </BrowserRouter>
+
+  );
+
+}
+
+
+export default App;
