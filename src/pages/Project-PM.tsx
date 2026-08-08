18b8c3c9d67b86ff4477f0e8ad7af45daca66932
Yanisa V <yanisa8483@gmail.com>
2026-08-08 14:37:11 +0700

    link added

--- src/pages/Project-PM.tsx
+++ src/pages/Project-PM.tsx
@@ -1,7 +1,23 @@
 export function ProjectPM() {
   return (
     <>
-      <p className="eyebrow">ProjectPM</p>
+      <p className="eyebrow">Project preventive-maintenance</p>
+      <p>
+        Link:{" "}
+        <a
+          href="https://github.com/bowyani/preventive-maintenance"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Docs
+        </a>
+      </p>
+      <p>
+        Link:{" "}
+        <a href="#" target="_blank" rel="noopener noreferrer">
+          Prototype
+        </a>
+      </p>
     </>
   );
 }
