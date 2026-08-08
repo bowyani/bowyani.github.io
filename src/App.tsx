e0f630694f4af83c054e00b73ca14b8bac9441b0
Yanisa V <yanisa8483@gmail.com>
2026-08-08 14:22:36 +0700

    fix path

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { Home } from "@/pages/Home";
 import { ProjectPM } from "@/pages/Project-PM";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/pm" element={<ProjectPM />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
